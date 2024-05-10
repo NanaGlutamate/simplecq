2a5fdad6d3595860bd26cec00e8a584e3f915186
NanaGlutamate <30829895+NanaGlutamate@users.noreply.github.com>
2024-05-10 15:48:09 +0800

    0510

--- src/tinycq.cpp
+++ src/tinycq.cpp
@@ -419,8 +419,6 @@ struct CommandReceiver {
 };
 
 int main() {
-    // using namespace cq;
-
     TinyCQ cq;
     cq.cfg.log_file = std::ofstream(__PROJECT_ROOT_PATH "/config/log.txt");
     cq.cfg.log_level = 4;
@@ -432,25 +430,5 @@ int main() {
         // cq.printBuffer();
         cq.draw();
     }
-
-    // std::cout << std::format("[main]: {1}", std::this_thread::get_id(), "post") << std::endl;
-    // {
-    //     std::mutex mtx;
-    //     ThreadPool tp{8};
-    //     for (size_t i = 0; i < 10; i++) {
-    //         tp.post([&] {
-    //             for (auto i = 0; i < 10000; ++i) {
-    //                 {
-    //                     std::unique_lock lock{mtx};
-    //                     std::cout << std::format("[{}]: {}", std::this_thread::get_id(), i) << std::endl;
-    //                 }
-    //             }
-    //         });
-    //     }
-    //     // std::cout << std::format("[main]: {1}", std::this_thread::get_id(), "posted and wait") << std::endl;
-    //     tp.waitForEmpty();
-    //     // std::cout << std::format("[main]: {1}", std::this_thread::get_id(), "done") << std::endl;
-    // }
-    // std::cout << std::format("[main]: {1}", std::this_thread::get_id(), "end") << std::endl;
     return 0;
 }
