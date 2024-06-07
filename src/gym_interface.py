afae14c9071e39649c65d6d3769bc501b4e954f6
NanaGlutamate <30829895+NanaGlutamate@users.noreply.github.com>
2024-06-07 13:07:54 +0800

    debug, add comments

--- src/gym_interface.py
+++ src/gym_interface.py
@@ -5,6 +5,7 @@ def make_xml_data(data, type_def) -> str:
     match type_def:
         case str(atomic_type):
             assert atomic_type in {"bool", "int8_t", "uint8_t", "int16_t", "uint16_t", "int32_t", "uint32_t", "int64_t", "uint64_t", "float", "double"}
+            if atomic_type == 'bool': return f'<{atomic_type}>{1 if data else 0}</{atomic_type}>'
             return f'<{atomic_type}>{data}</{atomic_type}>'
         case list(l):
             return f'<li>{''.join(make_xml_data(value, l[0]) for value in data)}</li>'
@@ -17,10 +18,11 @@ class State(Enum):
     IS_END = 3
 
 class Agent:
-    def __init__(self, outputs_type, port, host='localhost', restart_key='restart', process_output=None, reward_func=None, end_func=None) -> None:
+    def __init__(self, outputs_type, port, host='localhost', restart_key='restart', 
+                 process_input=None, process_output=None, reward_func=None, end_func=None) -> None:
         '''
-            outputs_type: type of output data, need for communicate with c++
-                example for outputs_type: {'location' : {'x' : 'double', 'y' : 'double', 'z' : 'double'}, 'enemiey_ids' : ['uint64_t']}
+            outputs_type: 输出数据的类型标注信息.
+                例子: {'location' : {'x' : 'double', 'y' : 'double', 'z' : 'double'}, 'enemiey_ids' : ['uint64_t']}
             
         '''
         s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -31,22 +33,31 @@ class Agent:
         self.state = State.FIRST_INIT
         self.restart_key = restart_key
 
+        self.process_input = lambda x:x if process_input is None else process_input
         self.process_output = lambda x:x if process_output is None else process_output
         self.cal_reward = lambda x:0 if reward_func is None else reward_func
         self.cal_end = lambda x:False if end_func is None else end_func
 
+        self.init_val = None
+
     def reset(self):
         if self.state == State.FIRST_INIT:
             self.state = State.RUNNING
-            return self._recv()
-        elif self.state == State.IS_END or State.RUNNING:
+            self.init_val = self._recv()
+            return self.init_val
+        elif self.state == State.IS_END or self.state == State.RUNNING:
             self.state = State.RUNNING
             self._restart()
-            return self._recv()
+            self._recv()
+            return self.init_val
 
     def step(self, action):
+        '''
+            注意: 由于CQSim执行模型的问题, step返回的state并不是当前传入的动作的结果, 而是上一个动作的结果
+                即: 当前帧的action并不会影响step返回的state, 而只能对下一帧的step返回值产生影响
+        '''
         assert self.state == State.RUNNING
-        self._send(action)
+        self._send(self.process_input(action))
         s = self.process_output(self._recv())
         end = self.cal_end(s)
         if end:
@@ -60,7 +71,7 @@ class Agent:
         s = make_xml_data(data, self.outputs_type)
         self.link.send(s.encode() + b'\n')
 
-    def _recv(self)->str:
+    def _recv(self):
         s: bytes = b''
         while not s.endswith(b'\n'):
             s = s + self.link.recv(1024)
